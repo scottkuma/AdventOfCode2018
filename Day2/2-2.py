edbfb8100af6cba5e3682b518a545d3be4c8e22a
scottkuma <scott@scottkuma.net>
2018-12-02 22:55:56 +0000

    slight optimize

--- Day2/2-2.py
+++ Day2/2-2.py
@@ -4,11 +4,11 @@ def getCommonLetters(word1, word2):
 		if word1[p] == word2[p]:
 			letters += word1[p]
 	return letters
-	
+
 
 with open("2-input.txt") as f:
 	content = f.readlines()
-	
+
 box_list = [x.strip() for x in content]
 box_list.sort()
 
@@ -19,9 +19,7 @@ for box in box_list:
 	else:
 		cl = getCommonLetters(box,prev_box)
 		if  len(box) - len(cl) == 1:
-			print( getCommonLetters(box, prev_box) )
+			print( cl )
 			exit()
 		else:
 			prev_box = box
-			
-		

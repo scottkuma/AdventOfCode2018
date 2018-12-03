29cb5723024385635ef183df6df8ed1014d226f2
scottkuma <scott@scottkuma.net>
2018-12-03 13:37:20 +0000

    Commenting & output formatting.

--- Day3/3.py
+++ Day3/3.py
@@ -17,27 +17,32 @@ max_x = 0
 max_y = 0
 
 with open("3-input.txt") as f:
+    # Find Max x and y dimensions (how big does the cloth need to be?)
+    # While I'm at it, store the claims so I don't need to read the file again.
     for line in f:
         g = re.search(r"(\d+) @ (\d+),(\d+): (\d+)x(\d+)", line)
         num,x,y,w,h = g.groups()
-        #print(x,y,w,h)
         if int(x) + int(w) > max_x:
             max_x = int(x) + int(w)
         if int(y) + int(h) > max_y:
             max_y = int(y) + int(h)
         claims.append(Claim(num,x,y,w,h))
 
-#print(max_x,max_y)
-
+# Make my cloth representation
 cloth = np.zeros((max_y, max_x))
 
+# Cycle through all claims, incrementing each sq in
+# within each claim.
 for c in claims:
     for x in range(c.x,c.x + c.w):
         for y in range(c.y,c.y+c.h):
             cloth[y][x] += 1
 
-print len(cloth[np.where(cloth > 1)])
+# Use numpy function magic to find how many sq in have multiple claims
+print("Sq. In of overlapping cloth: {}".format(len(cloth[np.where(cloth > 1)])))
 
+# Cycle through all claims again, looking to see if multiple claims
+# exist in them.  If none, print the claim # (and keep going in case > 1)
 for c in claims:
     for x in range(c.x,c.x + c.w):
         for y in range(c.y,c.y+c.h):
